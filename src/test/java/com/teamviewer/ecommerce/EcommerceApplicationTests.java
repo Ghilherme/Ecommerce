2b9ee06846cb0a9f05f24b89e35777e28d81b288
Guil <g.p.oliveira@hotmail.com>
2024-07-26 20:46:48 -0300

    adding integration tests

--- src/test/java/com/teamviewer/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/teamviewer/ecommerce/EcommerceApplicationTests.java
@@ -1,11 +1,9 @@
 package com.teamviewer.ecommerce;
 
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
