ee1d66a8b8ec3df95b5208a0789263cfe9675779
Guil <g.p.oliveira@hotmail.com>
2024-07-27 16:50:05 -0300

    adding validations

--- src/main/java/com/teamviewer/ecommerce/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/teamviewer/ecommerce/exceptions/ControllerExceptionHandler.java
@@ -2,24 +2,13 @@ package com.teamviewer.ecommerce.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex, WebRequest request) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
 
     @ExceptionHandler(InvalidDataException.class)
     public ResponseEntity<ErrorMessageApi> handleInvalidDataException(InvalidDataException ex) {
